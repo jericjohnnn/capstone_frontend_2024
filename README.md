92eab6ff47f582ac31ff5c74ebd7551be567e323
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2024-11-22 22:54:56 +0800

    build for production

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## 🚀 How to Install
 
-1. `cd frontend`
 2. `npm install`
 3. `npm run dev`
 
--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // Your API base URL
+  baseURL: 'https://tudlo.cctcccs.com/', 
 });
 
 // Add a request interceptor
