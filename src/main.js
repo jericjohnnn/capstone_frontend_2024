4750c7adb24c62880a07f5453e156ab8844672a4
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-02-18 17:52:11 +0800

    mura kag amaw

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-import "preline/preline";
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 
